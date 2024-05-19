175ad7b610cb5bd94f2819723c9d81d56534b011
Tupitsyn Aleksandr <aleksandr.tupitcyn@mostgames.org>
2024-05-19 21:57:47 +0900

    Only redirectiong from dashboard.

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 
 export default auth((req) => {
-    if (!req.auth) {
+    if (!req.auth && req.nextUrl.pathname.startsWith("/dashboard")) {
         const url = req.url.replace(req.nextUrl.pathname, "/login");
         return Response.redirect(url);
     }
