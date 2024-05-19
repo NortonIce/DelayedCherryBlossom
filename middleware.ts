fabb267fbc125badc9cd5b0572df150f8cd59a4a
Tupitsyn Aleksandr <aleksandr.tupitcyn@mostgames.org>
2024-05-19 20:59:49 +0900

    Middleware redirect

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,8 @@
+import { auth } from "@/auth";
 
-
-
-export { auth as middleware } from "@/auth"
+export default auth((req) => {
+    if (!req.auth) {
+        const url = req.url.replace(req.nextUrl.pathname, "/login");
+        return Response.redirect(url);
+    }
+});
