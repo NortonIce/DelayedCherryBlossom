9afc087294aa331c69ff34ced817bae9e092c554
Tupitsyn Aleksandr <aleksandr.tupitcyn@mostgames.org>
2024-05-19 22:05:44 +0900

    Moved goolge login to the login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,10 @@
 import LoginForm from "../ui/login-form";
+import { SignIn } from "../ui/sign-in";
 
 export default function LoginPage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <SignIn />
             <button>Login</button>
 
             <LoginForm />
