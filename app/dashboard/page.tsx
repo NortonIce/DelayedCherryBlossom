f724a4508a551e816fd28550367c8dd21ab20c6a
Tupitsyn Aleksandr <aleksandr.tupitcyn@mostgames.org>
2024-05-19 20:54:40 +0900

    Sign out

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 import Image from "next/image";
 
 export default async function Dashboard() {
@@ -12,6 +12,15 @@ export default async function Dashboard() {
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h3>Dashboard</h3>
             <p>{session.user?.email}</p>
+
+            <form
+                action={async () => {
+                    "use server";
+                    await signOut();
+                }}
+            >
+                <button type="submit">Sign Out</button>
+            </form>
         </main>
     );
 }
