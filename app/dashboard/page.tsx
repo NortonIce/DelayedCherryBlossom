84e6d2df75616cb4573c93658a0ec70eae9f6a85
Tupitsyn Aleksandr <aleksandr.tupitcyn@mostgames.org>
2024-05-19 19:57:32 +0900

    Getting user from session

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,17 @@
+import { auth } from "@/auth";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Dashboard() {
+    const session = await auth();
+
+    if (!session) {
+        return <div>No session</div>;
+    }
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
             <h3>Dashboard</h3>
+            <p>{session.user?.email}</p>
         </main>
     );
 }
--- auth.ts
+++ auth.ts
@@ -2,5 +2,8 @@ import NextAuth from "next-auth";
 import google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    session: {
+        strategy: "jwt",
+    },
     providers: [google],
 });
